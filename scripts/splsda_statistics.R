df3e15b482dba2db10a40fc882e6f2aded2e2d32
cedunia <cedric.hassenkhodja@gmail.com>
2016-06-17 15:05:17 +0200

    add sPLS-DA method

--- scripts/splsda_statistics.R
+++ scripts/splsda_statistics.R
@@ -5,5 +5,10 @@ source("scripts/splsda_cleaning.R")
 library(mixOmics)
 
 ## ORGANIZE DATA ##
-data_stats <- X
-data_stats.pca <- pca(X, ncomp = 3, center = TRUE, scale = TRUE)
+data_stats.X <- X
+data_stats.Y <- Y
+
+## BUILD MODEL ##
+data_stats.pca <- pca(data_stats.X, ncomp = 3, center = TRUE, scale = TRUE)
+
+data_stats.splsda <- splsda(data_stats.X,data_stats.Y, ncomp = 3, keepX = c(50,50,50))
