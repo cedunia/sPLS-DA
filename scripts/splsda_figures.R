9674ac0eaa93b83655ff878a7d865c13c2227a02
cedunia <cedric.hassenkhodja@gmail.com>
2016-06-17 14:26:56 +0200

    made figures script

---
+++ scripts/splsda_figures.R
@@ -0,0 +1,20 @@
+## READ IN DATA ##
+source("./scripts/splsda_statistics.R")
+
+## LOAD PACKAGE ##
+library(mixOmics)
+
+## ORGANIZE DATA ##
+cols <- c('#e41a1c','#377eb8','#4daf4a','#984ea3')
+data_figs.col <- factor(srbct$class, levels = cols)
+data_figs.col <- as.numeric(as.factor(srbct$class))
+data_figs.col <- ifelse(data_figs.col == 1, cols[1],
+                        ifelse(data_figs.col == 2, cols[2],
+                               ifelse(data_figs.col == 3, cols[3], 
+                                      cols[4])))
+data_figs <- data_stats.pca
+
+pdf('./figures/pca.pdf')
+plotIndiv(data_figs, col = data_figs.col, ind.names = FALSE, pch = 16)
+dev.off()
+
