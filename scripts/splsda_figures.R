108e4e073a1579a11ad5d4400ca8b9e893e7cda4
cedunia <cedric.hassenkhodja@gmail.com>
2016-06-17 16:48:49 +0200

    update script. Add correlation circle plot

--- scripts/splsda_figures.R
+++ scripts/splsda_figures.R
@@ -14,6 +14,7 @@ data_figs.col <- ifelse(data_figs.col == 1, cols[1],
                                       cols[4])))
 data_figs.pca <- data_stats.pca
 data_figs.splsda <- data_stats.splsda
+data_figs.plsda <- data_stats.plsda
 
 ## MAKE FILGURES ##
 pdf('./figures/pca.pdf')
@@ -29,4 +30,17 @@ dev.off()
 #In 3D
 plotIndiv(data_figs.splsda, comp = 1:3, ind.names = FALSE, col = data_figs.col,
           cex = 0.25, axes.box = "both", style = "3d")
+
+pdf('./figures/plsda.pdf')
+plotIndiv(data_stats.plsda, comp = 1:2, ind.names = FALSE, 
+          col = data_figs.col, pch = 16)
+dev.off()
+
+pdf('./figures/splsda_var.pdf')
+plotVar(data_figs.splsda, comp = 1:2, var.names = list(srbct$gene.name[,1]))
+dev.off()
+
+#3D
+plotVar(result, comp = 1:3, var.names  = list(srbct$gene.name[, 1]),
+        style='3d',axes.box = 'axes')
 
